1b78c6e3539c78223c319d69113f50a4471ed570
reddaGz <44gebre@gmail.com>
2021-05-21 13:51:11 -0500

    updated  Exercise03.ts

--- CS445Assigment/lab3/src/Exercise03.ts
+++ CS445Assigment/lab3/src/Exercise03.ts
@@ -15,17 +15,24 @@ console.log(car.acceleration); // 0
 car.accelerate(60); 
 console.log(car.acceleration); // 60
  */
-function Car(this:any,name:string) { 
-	this.name = name; 
-	this.acceleration= 0; 
-	this.honk = function() { 
-		console.log(` ${this.name} is saying: Toooooooooot!`); 
-	}; 
-	this.accelerate = function(speed:number) { 
+interface Vehicle{
+	name:string;
+	acceleration:number;
+	 honk():string;
+	 accelerate(speed:number):void;
+}
+class Car implements Vehicle{
+    constructor(public name:string,public acceleration:number=0 ){
+
+	}
+	honk():string{
+	 return ` ${this.name} is saying: Toooooooooot!`	
+	}
+	accelerate(speed:number):void{
 		this.acceleration = this.acceleration + speed; 
-	} 
-} 
-var car = new (Car as any)("BMW"); 
+	}
+}
+var car = new Car ("BMW"); 
 car.honk(); // BMW is saying: Toooooooooot!
 console.log(car.acceleration); // 0
 car.accelerate(60); 
