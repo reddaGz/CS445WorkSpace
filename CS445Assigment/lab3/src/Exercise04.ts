b5c8977a06b236be6c6b5e2685e5c9021ace34d3
reddaGz <44gebre@gmail.com>
2021-05-21 14:04:36 -0500

    updated Exercise04.ts

--- CS445Assigment/lab3/src/Exercise04.ts
+++ CS445Assigment/lab3/src/Exercise04.ts
@@ -13,20 +13,22 @@ rectangle.calcSize = function() {
 }; 
 
 console.log(rectangle.calcSize()); // 10
- */
-let baseObject:{
-    width:number,
-    length:number;
-} 
- baseObject = { 
-	width: 0, 
-	length: 0 
-}; 
-let  rectangle = Object.create(baseObject); 
-rectangle.width = 5; 
-rectangle.length = 2; 
-
-rectangle.calcSize = function():number { 
-	return this.width * this.length; 
-}; 
+ */ 
+ class BaseObject{
+	width:number;
+	length:number
+	constructor(width:number=0,length:number=0){
+		this.width=width;
+		this.length=length;
+	}
+}
+class Rectangle extends BaseObject{
+	constructor(width:number,lenght:number){
+		super(width,length)
+	}
+	calcSize():number{
+		return this.width * this.length; 
+	}
+}
+let rectangle=new Rectangle(5,2)
 console.log(rectangle.calcSize()); // 10
